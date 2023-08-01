2d537acfc948165344ffac917f4de69882041f8a
sbx_user1051 <sbx_user1051@169.254.71.233>
2023-08-01 16:06:02 +0000

    Update requested for Proton - environment/home-lab

---
+++ home-lab/provider.tf
@@ -0,0 +1,24 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:us-east-1:705740530616:environment/home-lab
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = var.proton_tags
+  }
+}
