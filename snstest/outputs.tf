edb96017a300dfa60d756c0f13c318242fab9765
sbx_user1051 <sbx_user1051@169.254.65.173>
2023-08-03 15:46:01 +0000

    Update requested for Proton - environment/snstest

---
+++ snstest/outputs.tf
@@ -0,0 +1,11 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:us-west-2:705740530616:environment/snstest
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+output "SnsRegion" {
+  value = local.region
+}
